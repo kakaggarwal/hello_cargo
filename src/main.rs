97260f513df0010c802ee9c78a754b2893cf8350
Krishan Aggarwal <kakaggarwal@gmail.com>
2023-01-22 19:52:37 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-
     println!("Please input your guess.");
+
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("You guessed: {guess} and the secret number is: {secret_number}");
 }
