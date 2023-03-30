e7e11d67f93aebe6bda71889ea0f7b6461c562d1
Krishan Aggarwal <kakaggarwal@gmail.com>
2023-03-30 22:44:22 +0530

    added match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io;
 use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Guess the number!");
@@ -11,7 +11,18 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("You guessed: {guess} and the secret number is: {secret_number}");
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+    println!(
+        "You guessed: {guess} and the secret number is: {}",
+        secret_number
+    );
 }
