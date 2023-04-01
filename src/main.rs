0c9248c34468e8f8ef63149a501516c1ae626372
Krishan Aggarwal <kakaggarwal@gmail.com>
2023-04-01 17:21:41 +0530

    mutability and shadowing added

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,43 @@ use rand::Rng;
 use std::{cmp::Ordering, io};
 
 fn main() {
+    loop {
+        println!("Select from below, enter 0 to exit.");
+        println!("1. Guessing Game");
+        println!("2. Mutability");
+        println!("3. Shadowing");
+        print!("Enter your choice: ");
+
+        let mut choice = String::new();
+
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to readline");
+
+        let choice: u32 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match choice {
+            1 => guess_game(),
+            2 => mutability(),
+            3 => shadowing(),
+            _other => break,
+        };
+
+        println!("=============================================");
+    }
+}
+
+fn guess_game() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
+
     loop {
         println!("Please input your guess.");
-        
+
         let mut guess = String::new();
 
         io::stdin()
@@ -29,4 +59,25 @@ fn main() {
             }
         }
     }
+}
+
+fn mutability() {
+    let mut x = 5;
+    println!("The value of x is {x}");
+
+    x = 6;
+    println!("The value of x is {x}");
+}
+
+fn shadowing() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("Value of x in the inner scope is: {x}");
+    }
+
+    println!("Value of x is: {x}");
 }
